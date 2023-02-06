8acc4f397264e9330974b73a7c6f82be560c162b
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-06 10:54:25 -0300

    Add new project as Domain

---
+++ ShopFast.Domain/Client.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ShopFast.Domain
+{
+    class Client
+    {
+        public Client()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
