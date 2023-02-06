8254121ce5321e7beecb6d40f5e9a58c3a27da19
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-06 12:20:13 -0300

    Add mappings to DbContext

--- ShopFast/Context/FastShopDataContext.cs
+++ ShopFast/Context/FastShopDataContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Entity;
 using ShopFast.Domain;
+using ShopFast.Mapping;
 
 namespace ShopFast.Context
 {
@@ -8,6 +9,13 @@ namespace ShopFast.Context
         public FastShopDataContext()
             :base("FastShopConnectionString")
         {}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ClientMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Client> Clients { get; set; }
