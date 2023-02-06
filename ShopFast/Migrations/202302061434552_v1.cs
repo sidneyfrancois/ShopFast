05db4241fe5d24c8728ad3cfad11ea6a4111bf54
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-06 11:37:14 -0300

    Add first migration and connection string

---
+++ ShopFast/Migrations/202302061434552_v1.cs
@@ -0,0 +1,41 @@
+﻿namespace ShopFast.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ClientId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clients", t => t.ClientId, cascadeDelete: true)
+                .Index(t => t.ClientId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "ClientId", "dbo.Clients");
+            DropIndex("dbo.Products", new[] { "ClientId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Clients");
+        }
+    }
+}
---
+++ ShopFast/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ShopFast.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ShopFast.Context.FastShopDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ShopFast.Context.FastShopDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
